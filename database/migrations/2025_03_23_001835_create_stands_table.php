0a2ec700e8e2ffc440fc1d3e3fa472634d7daac0
DanielvisRamos <danielvisramos31@mail.com>
2025-03-22 21:10:31 -0400

    first commit

---
+++ database/migrations/2025_03_23_001835_create_stands_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('evento_id')->constrained('eventos')->onDelete('cascade');
+            $table->string('nombre');
+            $table->decimal('precio', 10, 2);
+            $table->enum('estado', ['disponible', 'reservado', 'ocupado'])->default('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stands');
+    }
+};
---
+++ database/migrations/2025_03_23_001940_create_reembolsos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reembolsos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pago_id')->constrained('pagos')->onDelete('cascade');
+            $table->decimal('monto', 10, 2);
+            $table->text('razon');
+            $table->enum('estado', ['pendiente', 'completado'])->default('pendiente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reembolsos');
+    }
+};
